0fde31b7d4f59519b8eaf807d72e45940a025749
Yevhenii Obiedkov <yevhenii.obiedkov@LT-M-6919.local>
2024-08-20 16:30:37 +0300

    version 2.0

--- src/main/java/com/codefinity/DonationTaskJoin.java
+++ src/main/java/com/codefinity/DonationTaskJoin.java
@@ -2,14 +2,14 @@ package com.codefinity;
 
 import java.util.concurrent.RecursiveTask;
 
-public class TaskForkJoin extends RecursiveTask<Long> {
+public class DonationTaskJoin extends RecursiveTask<Long> {
     private static final int THRESHOLD = 200; // Threshold for task splitting
-    private final long[] array;
+    private final long[] listDonations;
     private final int start;
     private final int end;
 
-    public TaskForkJoin(long[] array, int start, int end) {
-        this.array = array;
+    public DonationTaskJoin(long[] listDonations, int start, int end) {
+        this.listDonations = listDonations;
         this.start = start;
         this.end = end;
     }
